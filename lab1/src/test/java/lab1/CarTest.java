eae8ea95e8438a647c1262dd79a0d30e50556b71
Robin Hallabro <robin@hallabro.nu>
2018-11-08 14:50:18 +0100

    lab1: Rename tests to reflect what is actually being tested

--- lab1/src/test/java/lab1/CarTest.java
+++ lab1/src/test/java/lab1/CarTest.java
@@ -15,93 +15,90 @@ public class CarTest {
   }
 
   @Test
-  public void getNumberOfDoors() {
+  public void numberOfDoorsShouldEqualFour() {
     assertEquals(4, this.car.getNumberOfDoors());
   }
 
   @Test
-  public void getEnginePower() {
+  public void enginePowerShouldEqualZero() {
     assertEquals(0, this.car.getCurrentSpeed());
   }
 
   @Test
-  public void getCurrentSpeed() {
+  public void currentSpeedShouldEqualZero() {
     assertEquals(0, this.car.getCurrentSpeed());
   }
 
   @Test
-  public void getColor() {
+  public void colorShouldBeSet() {
     this.car.setColor(Color.BLUE);
     assertEquals(Color.BLUE, this.car.getColor());
   }
 
   @Test
-  public void setColor() {
-    this.car.setColor(Color.blue);
-    assertEquals(Color.blue, this.car.getColor());
-  }
+  public void startEngineShouldIncreaseSpeed() {
+    assertEquals(0, this.car.currentSpeed, "guard, car should be at standstill");
 
-  @Test
-  public void startEngine() {
     this.car.startEngine();
     assertEquals(0.1, this.car.currentSpeed);
   }
 
   @Test
-  public void stopEngine() {
+  public void stopEngineShouldStopCar() {
     this.car.startEngine();
-    assertTrue(0 < this.car.currentSpeed, "guard");
+    assertTrue(0 < this.car.currentSpeed, "guard, car should not be at standstill");
 
     this.car.stopEngine();
     assertEquals(0, this.car.currentSpeed);
   }
 
   @Test
-  public void incrementSpeed() {
+  public void incrementSpeedShouldIncreaseSpeed() {
     this.car.startEngine();
-    this.car.incrementSpeed(10);
+    assertEquals(0.1, this.car.currentSpeed, "guard, car should not be at standstill");
 
+    this.car.incrementSpeed(10);
     assertEquals(10.1, this.car.currentSpeed);
   }
 
   @Test
-  public void decrementSpeed() {
+  public void decrementSpeedShouldDecreaseSpeed() {
     this.car.startEngine();
-    assertTrue(0.1 <= this.car.currentSpeed);
+    assertEquals(0.1, this.car.currentSpeed, "guard, car should not be at standstill");
 
     this.car.decrementSpeed(0.1);
     assertEquals(0, this.car.currentSpeed);
   }
 
   @Test
-  public void getX() {
+  public void xCoordinateShouldBeZero() {
     assertEquals(0, this.car.getX());
   }
 
   @Test
-  public void getY() {
+  public void yCoordinateShouldBeZero() {
     assertEquals(0, this.car.getY());
   }
 
   @Test
-  public void move() {
+  public void moveShouldMoveCarInCurrentDirection() {
     this.car.incrementSpeed(10);
-    assertTrue(10 <= this.car.getCurrentSpeed());
+    assertTrue(10 <= this.car.getCurrentSpeed(), "guard, car should not be at standstill");
 
     this.car.move();
 
     assertEquals(10, this.car.y);
-    assertEquals(0, this.car.x, "guard, default direction is up");
+    assertEquals(0, this.car.x, "default direction is up, x should not be affected");
   }
 
   @Test
-  public void turnLeft() {
+  public void turnLeftShouldTurnCarLeft() {
     this.car.turnLeft();
     assertEquals(Car.Direction.LEFT, this.car.direction);
   }
 
   @Test
-  public void turnRight() {
+  public void turnRightShouldTurnCarRight() {
     this.car.turnRight();
     assertEquals(Car.Direction.RIGHT, this.car.direction);
   }
