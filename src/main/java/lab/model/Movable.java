ae8a96c19a7ff550c0ab3d0bd89d16da3d66314c
Robin Hallabro <robin@hallabro.nu>
2018-11-28 17:45:15 +0100

    Rename package from lab3 to lab

---
+++ src/main/java/lab/model/Movable.java
@@ -0,0 +1,14 @@
+package lab.model;
+
+/** Generic movable. */
+public interface Movable {
+  /** Moves the movable in the current direction. */
+  public void move();
+
+  /**
+   * Pushes the movable 1 unit in the given direction.
+   *
+   * @param direction the direction in which to push the Movable.
+   */
+  public void pushInDirection(final Direction direction);
+}
---
+++ src/test/java/lab/model/vehicles/CarStub.java
@@ -0,0 +1,15 @@
+package lab.model.vehicles;
+
+import java.awt.Color;
+
+public class CarStub extends Car {
+
+  public CarStub() {
+    super(4, Color.red, 10, "Test");
+    stopEngine();
+  }
+
+  public double speedFactor() {
+    return 1;
+  }
+}
