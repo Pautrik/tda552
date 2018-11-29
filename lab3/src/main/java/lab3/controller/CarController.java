7cc71f00e572edb3ae1e709beb72d88992718cb7
raoulkent <raoulkent@gmail.com>
2018-11-29 13:57:45 +0100

    Moved logic out of the main method in CarController into it's constructor

--- lab3/src/main/java/lab3/controller/CarController.java
+++ lab3/src/main/java/lab3/controller/CarController.java
@@ -27,22 +27,18 @@ public class CarController {
 
     // The frame that represents this instance View of the MVC pattern
     private CarView frame;
-    // A list of cars, modify if needed
-    private ArrayList<Car> cars = new ArrayList<>();
 
-    //methods:
+    // Model of the world containing all cars and the space they exist in.
+    private World world;
+
+    public CarController() {
+        this.world = new World();
+        this.frame = new CarView("CarSim 1.0", this);
+        this.timer.start();
+    }
 
     public static void main(String[] args) {
-        // Instance of this class
         CarController cc = new CarController();
-
-        cc.cars.add(new Volvo240());
-
-        // Start a new view and send a reference of self
-        cc.frame = new CarView("CarSim 1.0", cc);
-
-        // Start the timer
-        cc.timer.start();
     }
 
     /* Each step the TimerListener moves all the cars in the list and tells the
@@ -50,10 +46,10 @@ public class CarController {
     * */
     private class TimerListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            for (Car car : cars) {
-                car.move();
-                int x = (int) Math.round(car.getPosition().getX());
-                int y = (int) Math.round(car.getPosition().getY());
+            for (Vehicle vehicle : world.getVehicles()) {
+                vehicle.move();
+                int x = (int) Math.round(vehicle.getPosition().getX());
+                int y = (int) Math.round(vehicle.getPosition().getY());
                 frame.drawPanel.moveit(x, y);
                 // repaint() calls the paintComponent method of the panel
                 frame.drawPanel.repaint();
@@ -64,15 +60,15 @@ public class CarController {
     // Calls the gas method for each car once
     public void gas(int amount) {
         double gas = ((double) amount) / 100;
-        for (Car car : cars) {
-            car.gas(gas);
+        for (Vehicle vehicle : world.getVehicles()) {
+            vehicle.gas(gas);
         }
     }
 
     public void brake(int amount) {
         double brake = ((double) amount) / 100;
-        for (Car car : cars) {
-            car.brake(brake);
+        for (Vehicle vehicle : world.getVehicles()) {
+            vehicle.brake(brake);
         }
     }
 
